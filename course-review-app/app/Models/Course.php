952db0005ec223e3403c655c0245f53cfa6b13df
Miguel-Ramirez-256 <ramirezmiguelan1993@gmail.com>
2025-11-08 23:19:57 -0600

    Creacion de Modelos y Migraciones

---
+++ course-review-app/app/Models/Course.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Course extends Model
+{
+    use HasFactory;
+    public function reviews() { return $this->hasMany(Review::class); }
+    public function getRouteKeyName() { return 'slug'; }
+}
--- course-review-app/app/Models/User.php
+++ course-review-app/app/Models/User.php
@@ -12,6 +12,8 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
+    public function reviews() { return $this->hasMany(Review::class); }
+
     /**
      * The attributes that are mass assignable.
      *
